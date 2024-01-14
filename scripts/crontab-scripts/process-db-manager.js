7bd5cecd37b0240b802a77a3fb8b15e6772e19dd
Alvaro <alvar01omer@gmail.com>
2024-01-14 00:21:02 -0300

    server

--- scripts/crontab-scripts/process-db-manager.js
+++ scripts/crontab-scripts/process-db-manager.js
@@ -1,50 +1,126 @@
-const db = require('../../models/index');
-const { spawn } = require('child_process');
+const db = require("../../models/index");
+const { spawn } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
 const startPort = 3000;
-const endPort = 4000;
-
+const endPort = 3010;
+const childProcesses = []; // Array to store child processes
 
 function findAvailablePort(startPort, endPort, directory, appJSFile) {
-    return new Promise((resolve, reject) => {
-        const server = exec(`POST=${startPort} node ${directory} ${appJSFile}`);
+  return new Promise((resolve, reject) => {
+    let processData = {
+      PID: null,
+      PORT: null,
+    };
 
-        server.on('error', (err) => {
-            if (err.code === 'ENOENT') {
-                resolve(startPort);
-            } else {
-                reject(err);
-            }
-        });
+    processData.PORT = startPort;
 
-        server.stdout.on('data', (data) => {
-            if (data) {
-                findAvailablePort(startPort + 1, endPort)
-                    .then(resolve)
-                    .catch(reject);
-            } else {
-                resolve(startPort);
-            }
-        });
+    console.log(
+      `running command: PORT=${startPort} node ${directory}/${appJSFile}`
+    );
 
-        server.on('exit', (code) => {
-            if (code !== 0) {
-                reject(new Error(`Command exited with code ${code}`));
-            }
-        });
+    const childProcess = spawn("node", [path.join(directory, appJSFile)], {
+      env: {
+        ...process.env,
+        PORT: startPort.toString(),
+      },
     });
+
+    childProcess.on("spawn", () => {
+      console.log("Child Process spawned with PID:", childProcess.pid);
+      processData.PID = childProcess.pid;
+    });
+
+    childProcesses.push({
+      process: childProcess,
+      port: startPort,
+      pid: childProcess.pid,
+      finishFlag: false,
+    }); // Store the child process in the array
+
+    childProcess.on("error", (err) => {
+      if (err.code === "ENOENT") {
+        console.log("r1", err);
+        resolve(startPort);
+      } else {
+        reject(err);
+      }
+    });
+
+    childProcess.stderr.on("data", (data) => {
+      console.error("Child Process stderr:", data.toString());
+    });
+
+    childProcess.stdout.on("data", (data) => {
+      console.log("Child Process stdout:", data.toString());
+    });
+
+    childProcess.on("exit", (code, signal) => {
+      if (code !== 0) {
+        reject(new Error(`Command exited with code ${code}`));
+      } else if (signal === "SIGINT") {
+        console.log("Child Process terminated due to SIGINT signal");
+        // Perform cleanup or additional processing here
+      } else {
+        console.log("Child Process completed successfully");
+        // Perform cleanup or additional processing here
+      }
+    });
+
+    process.on("SIGINT", () => {
+      // Perform cleanup or additional processing here
+      // Terminate all child processes
+      const childProcessPromises = childProcesses.map((cp) => {
+        return new Promise((resolve) => {
+          cp.process.on("exit", (code, signal) => {
+            resolve(cp);
+          });
+          cp.process.kill("SIGINT");
+        });
+      });
+
+      childProcessPromises.forEach((promise, index) => {
+        promise.then((cp) => {
+          if (!childProcesses[index].finishFlag) {
+            console.log(
+              "Child Process received SIGINT signal, System in port: " +
+                cp.port +
+                " PID: " +
+                cp.pid +
+                " Finished"
+            );
+            childProcesses[index].finishFlag = true;
+          }
+        });
+      });
+
+      Promise.all(childProcessPromises).then(() => {
+        process.exit();
+      });
+    });
+  });
 }
 
+db.sequelize.sync().then(async () => {
 
-db.sequelize.sync().then(() => {
-    findAvailablePort(startPort, endPort)
-    .then((port) => {
-      console.log(`Found available port: ${port}`);
+    const sites = await db.Sites.findAll();
+    console.log(sites);
+  findAvailablePort(startPort, endPort, "repositories/app1", "index.js")
+    .then((data) => {
+      console.log(
+        `Found available port: ${data.PORT} with PID: ${data.PID}`,
+        data
+      );
+
       // Start your command/process on the found port in a separate thread
       // Retrieve the PID of the process using your desired method and handle as needed
     })
     .catch((err) => {
-      console.error('Failed to find available port:', err);
+      fs.appendFile("errors.log", err.toString(), (error) => {
+        if (error) {
+          console.error("Failed to log error:", error);
+        }
+      });
     });
-
 });
